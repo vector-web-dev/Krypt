05933891a7eea27cc857bf5fdc579403c1f6748c
Vector <vector02web@gmail.com>
2023-11-02 08:27:23 +0100

    krypt commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAbi = abi.abi;
+export const contractAddress = "0xC187459E9D3d15cEbBF858f978491E4799Fcd22D";
---
+++ client/src/utils/shortenAddress.js
@@ -0,0 +1,5 @@
+export const shortenAddress = (address) => `${address.slice(0, 6)}...${address.slice(address.length - 4)}`;
+
+// export function shortenAddress(address) {
+//     return `${address.slice(0, 6)}...${address.slice(address.length - 4)}`;
+// }
